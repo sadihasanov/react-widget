95bacd8630098e70bf2720c8f7db38ccbd4a793b
sadihasanov <62652308+sadihasanov@users.noreply.github.com>
2022-12-03 18:01:35 +0100

    Yet another test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,24 @@ function App() {
 
   return (
     <div className="reddit_widget__app">
-      <h1>Testing Number 2</h1>
+      <h1 className="reddit_widget__header">
+        Do I qualify for the orientation year permit?
+      </h1>
+      <div className="reddit_widget__body">
+        <div className="reddit_widget__selector">
+          <label htmlFor="grad_date">Graduation Date</label>
+          {/* {error && <p className="reddit_widget__error">{error}</p>} */}
+          <input
+            className={"reddit_widget__input"}
+            name="grad_date"
+            id="grad_date"
+            type="date"
+            placeholder="Enter your graduation date"
+            value={""}
+            onChange={() => {}}
+          />
+        </div>
+      </div>
     </div>
   );
 }
